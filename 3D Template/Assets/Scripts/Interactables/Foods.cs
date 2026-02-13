2f0d20d5205f9806482331ce70943cc95ca0f318
MUKILTEO\1725593 <camfry77@gmail.com>
2026-02-13 13:26:34 -0800

    Vending

---
+++ 3D Template/Assets/Scripts/Interactables/Foods.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
+
+public class Foods : Interactable
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected override void Interact()
+    {
+        
+    }
+}
+
